aac05f2ae601de7d1d8b34dce6d05569525743e4
thatneat <thatneat@users.noreply.github.com>
2016-08-23 23:00:24 -0700

    use .remove() instead of delete operator
    seems to work better on Windows 10/Chrome 52

--- rmspons.js
+++ rmspons.js
@@ -1,6 +1,6 @@
 var sug_posts = document.evaluate("//div[div[span[text()[contains(.,'Suggested Post')]]]]", document, null, XPathResult.ANY_TYPE, null );
 var sug_post = sug_posts.iterateNext();
 while(sug_post){
-    delete sug_post
+    sug_post.remove();
     sug_post = sug_posts.iterateNext();
 }
