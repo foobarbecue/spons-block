9bb0620a7a4787389d144a8a3e9f3f5134534b38
Aaron Curtis <aarongc@nmt.edu>
2016-08-25 00:20:24 -0600

    Typo in comment oh noes

--- rmspons.js
+++ rmspons.js
@@ -12,7 +12,7 @@ var remove_sug_posts = function(){
 }
 
 // Add event listener based on http://stackoverflow.com/a/27363569/616730
-// Triggers suggested post removal on when loading more timeline items
+// Triggers suggested post removal when loading more timeline items
 var origOpen = XMLHttpRequest.prototype.open;
 XMLHttpRequest.prototype.open = function() {
     this.addEventListener('load', remove_sug_posts);
